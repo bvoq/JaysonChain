c699536779b5823d393c4e48860991b10da2b066
Timofey Derkovskiy <dtimofey@ethz.ch>
2018-02-14 17:50:06 +0100

    basic api setup

--- API/index.js
+++
@@ -1,6 +0,0 @@
-var package = require(“. / package.json”);
-var api = require(“. / lib / api.js”);
-console.log(“loaded” + package.name + “, version” + package.version);
-exports.handler = function(event, context) {
-  myNewApi.handleRequest(event, context.done);
-}
--- API/test/api_test.js
+++
@@ -1,7 +0,0 @@
-var assert = require(“chai”).assert;
-var api = require(“.. / lib / api.js”);
-describe(“JaysonChainApi”, function() {
-  it(“exports handleRequest”, function() {
-    assert.typeOf(api.handleRequest, “function”);
-  });
-});
