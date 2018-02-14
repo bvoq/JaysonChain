d89024b3731eab216d286c507d2835878c31ec92
Simon Yuan <yuansi@student.ethz.ch>
2018-02-14 15:46:11 +0100

    lol

--- Code/JaysonChain.sol
+++ Code/JaysonChain.sol
@@ -4,5 +4,31 @@ import "./JSONcontract.sol";
 import "./Owned.sol";
 
 contract JaysonChain is Owned {
-    
+	struct action {
+		string type; //moved, consumed/sold, used(in machine) 
+		JSON data; //timestamp, location form to, temp, humidity,... etc
+	}
+	
+	struct Asset {
+		uint assetID;
+		string state; //consumed, defect, built-in, stolen/lost.
+		string location;
+		JSON data;
+		action[] actionhistory;
+	}
+	
+	struct prod{
+		uint[] inputs; //input AssetIDs
+		uint[] outputs; //output AssetIDs
+		JSON data; //addistional data (timestamp, etc)
+	}
+	
+
+	
+	
+	Asset[] allAssets;
+	
+	function createAsset() private {
+		allAssets.len
+	}
 }
