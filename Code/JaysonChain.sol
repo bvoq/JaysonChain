efe939cd27f7992240d87b26608df16aa5e8edba
kevin <dekeyser.kevin97@gmail.com>
2018-02-14 19:35:08 +0100

    added more stuff

--- Code/JaysonChain.sol
+++ Code/JaysonChain.sol
@@ -9,59 +9,94 @@ contract JaysonChain is Owned {
 	    _;
 	}
 	
+	
+	uint256 globalInternalTimeStamp = 0;
 	struct Attribute {
-	    uint attributeID;
+	    address author; //= msg.sender;
+	    uint256 attributeIndex;
+	    uint256 internalTimeStamp; //not unix time.
+		uint256 unixTime; // = now;
 	    string name;
 	    string data;
 	}
+		
+	function addActualAttribute(uint assetIndex, string name, string data) private {
+	    //TODO check if msg.sender may put name into allAssets[assetID].history, which we can check in permissionTable
+	    //TODO make sure allowance is deleted from permissionTable	
+	    Attribute newAttribute;
+	    newAttribute.author = msg.sender;
+	    newAttribute.attributeIndex = allAssets[assetIndex].history.length;
+	    newAttribute.internalTimeStamp = globalInternalTimeStamp;
+	    newAttribute.unixTime = now;
+	    newAttribute.name = name;
+	    newAttribute.data = data;
+	}
 	
+	//in order to add a single attribute 
+	function addAttribute(uint assetIndex, string name, string data) public {
+        addActualAttribute(assetIndex, name, data);
+	    globalInternalTimeStamp++;
+	}
 	
-	struct WriteEntry {
-	    string 
+		
+	function addAttributes(uint[] assetIndices, string[] attributeNames, string[] data) public {
+	    //here all have the same timestamp
+	    require(assetIndices.length == attributeNames.length && attributeNames.length == data.length);
+	    for(uint256 i = 0; i < assetIndices.length; ++i) addActualAttribute(assetIndices[i], attributeNames[i], data[i]);
+    	globalInternalTimeStamp++;
+	}
+	
+	function readAttribute(uint assetIndex, uint historyNo) public returns(Attribute) {
+	    return allAssets[assetIndex].history[historyNo];
+	}
+	
+	struct PermissionEntry {
+	    //TODO timeout
+	    string attributeNames;
 	}
 	
 	struct Asset {
-		uint256 assetID;
-		uint256 internalTime; //not unix time.
-		uint256 unixTime = now;
 	    address owner;
+		uint256 assetIndex;
 
 		Attribute[] history;
 		
-		mapping(address => string[]) permissionTable; //you can add attributes with the following names.
+		mapping(address => PermissionEntry[]) permissionTable; //you can add attributes with the following names. Later add timeout.
 	}
-	
-	
-	function allowWrite(uint assetID, address allowTo, string permissionNames) isAssetOwner(assetID){
-
-	}
-	
-	function allowWrite(uint assetID, address allowTo, string[] permissionNames) isAssetOwner(assetID){
-	    for(int i = 0; i < permissionNames.length; ++i) allowWrite(assetID, allowTo, permissionNames[i]);
-	}
-	
 	Asset[] allAssets;
-	
+
 	function createAsset() public {
 	    Asset newAsset;
-	    newAsset.blockChainId = msg.sender;
-	    newAsset.assetID = 12;
+	    newAsset.owner = msg.sender;
+	    newAsset.assetIndex = allAssets.length;
+	    allAssets.push(newAsset);
+	}
+
+	function allowWrite(uint assetIndex, address allowTo, string permissionName) private isAssetOwner(assetIndex) {
+	    PermissionEntry newPermissionEntry;
+	    newPermissionEntry.attributeNames = permissionName;
+        allAssets[assetIndex].permissionTable[allowTo].push(newPermissionEntry);
 	}
 	
-	function addAttribute(uint assetID, string name, string data) public {
-	    //TODO check if msg.sender may put name into allAssets[assetID].history, which we can check in permissionTable
-	    //TODO make sure allowance is deleted from writeTable
+	function allowWrite(uint assetIndex, address allowTo, string[] permissionNames) public isAssetOwner(assetIndex){ //todo make free for owner
+	    for(uint256 i = 0; i < permissionNames.length; ++i) allowWrite(assetIndex, allowTo, permissionNames[i]);
 	}
 	
-	function readAttribute(uint assetID, uint historyNo) public returns(Attribute) {
-	    return allAssets[assetID].history[historyNo];
+	function disallowAllWrites(uint assetIndex, address disallowTo) public isAssetOwner(assetIndex) { //TODO
+	    
 	}
 	
 	
+	//"verify" is an attribute
+	//"attributeX was wrong" is a new attribute 
+
 	
-	//consortium blockchain
 	
-	function createAsset() private {
-	}
+	
+
+
+	
+	
+	
 }
 
